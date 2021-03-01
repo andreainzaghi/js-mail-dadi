59e17d0bc34359d5077d99f9850aecedea7458ba
Andrea <andrea3aprile99@gmail.com>
2021-03-01 21:04:47 +0100

    e

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,25 +1,25 @@
 // controllo se e' un ekail
 // if(){
 
-  function validaEmail(email) {
-
-    var regexp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regexp.test(email);
-  }
-
-
-  function checkEmail(){
-    risultato = document.getElementById("risultato");
-    email = document.getElementById("email").value;
-    risultato.innerHTML = "";
-    if (validaEmail(email)) {
-      risultato.innerHTML = email + " indirizzo valido";
-      risultato.style.color = 'green';
-    } else {
-      risultato.innerHTML = email + " indirizzo non valido";
-      risultato.style.color = 'red';
-    }
-  }
+  // function validaEmail(email) {
+  //
+  //   var regexp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  //   return regexp.test(email);
+  // }
+  //
+  //
+  // function checkEmail(){
+  //   risultato = document.getElementById("risultato");
+  //   email = document.getElementById("email").value;
+  //   risultato.innerHTML = "";
+  //   if (validaEmail(email)) {
+  //     risultato.innerHTML = email + " indirizzo valido";
+  //     risultato.style.color = 'green';
+  //   } else {
+  //     risultato.innerHTML = email + " indirizzo non valido";
+  //     risultato.style.color = 'red';
+  //   }
+  // }
 // }
 
 // controllo array
